25e8f15a0b26a1194d6d641dc508afa60aecd7fd
I-Divine <igwedinmaifechukwu@gmail.com>
2023-09-14 12:12:12 +0100

    Integrated the advice API

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-document.getElementById("btn").onclick = async () => {
+let theData;
+const advice = document.getElementById("advice");
+const adviceId = document.getElementById("advice_id");
+document.getElementById("advice-btn").onclick = async () => {
+  advice.textContent = "...";
+  adviceId.textContent = `ADVICE -`;
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
-    // const movies = await response.json();
-    console.log(response.json());
+    response
+      .json()
+      .then((data) => {
+        theData = data;
+        console.log(theData);
+        console.log(theData.slip.advice);
+        advice.textContent = `"${theData.slip.advice}"`;
+        adviceId.textContent = `ADVICE ${theData.slip.id}`;
+      })
+      .catch((err) => console.log(err));
+    // console.log(theData);
   } catch (error) {
     console.log(error);
   }
-  console.log("pressed");
-
-  //   getadvice();
 };
-// async function getadvice() {}
